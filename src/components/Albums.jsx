c7959e9e39931e614222b968bb6e2c2d8f6385e1
martin_maseko <km.masekoccs@gmail.com>
2025-06-02 20:57:36 +0200

    paypal link updates

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -1,14 +1,23 @@
 import "./tapestyle.css";
 import IN4U from "./assets/IN4U.webp";
+import { useEffect } from "react";
 
 function Albums() {
+  useEffect(() => {
+    if (window.paypal && window.paypal.HostedButtons) {
+      window.paypal.HostedButtons({
+        hostedButtonId: "HVXKEJ5DGGQVJ",
+      }).render("#paypal-container-HVXKEJ5DGGQVJ");
+    }
+  }, []);
+
   return (
     <div>
       <h3>Albums</h3>
       <div className="albums">
         <div className="songs-list">
           <img src={IN4U} className="album-cover" alt="IN4U Album Cover" />
-          <div id="paypal-container-HVXKEJ5DGGQVJ"></div><div id="paypal-container-HVXKEJ5DGGQVJ"></div>
+          <div id="paypal-container-HVXKEJ5DGGQVJ"></div>
         </div>
       </div>
     </div>
