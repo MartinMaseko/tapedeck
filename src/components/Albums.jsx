83590128b608790ffd68fea7d69f99928ecc66a4
martin_maseko <km.masekoccs@gmail.com>
2025-06-02 20:49:21 +0200

    paypal link checks

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -7,7 +7,6 @@ function Albums() {
       <h3>Albums</h3>
       <div className="albums">
         <div className="songs-list">
-          <h4 className="catalogue-title">It's Not 4 U, It's for hustlers Vol.1</h4>
           <img src={IN4U} className="album-cover" alt="IN4U Album Cover" />
           <div id="paypal-container-HVXKEJ5DGGQVJ"></div><div id="paypal-container-HVXKEJ5DGGQVJ"></div>
         </div>
