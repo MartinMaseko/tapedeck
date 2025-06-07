2480e361ba65d21eafe8db80f57ba336f3bd6db6
martin_maseko <km.masekoccs@gmail.com>
2025-06-07 12:24:26 +0200

    Test port

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/apphealth', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
